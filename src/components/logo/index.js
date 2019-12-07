aa242f898e80e2aa1ea36f9d64588ca2cd9bb599
gubnich <gubnich@yandex.ru>
2019-12-07 19:06:31 +0300

    add components

---
+++ src/components/logo/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+export default () => {
+    return (
+        <h1 className='logo'>
+            <span>netflix</span>
+            <span>roulette</span>
+        </h1>
+    )
+}
---
+++ src/modules/main/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Header from '../../components/logo'
+
+export default () => {
+    return (
+        <Header />
+    )
+}
