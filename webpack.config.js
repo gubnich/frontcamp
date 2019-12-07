fca0be0ca2f4277ff4203d1c916a75fc0d310846
gubnich <gubnich@yandex.ru>
2019-12-07 19:09:51 +0300

    add copy plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.js",
@@ -33,6 +34,13 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html"
-    })
+    }),
+    new CopyPlugin([
+      {
+        from: 'src/static/images/*',
+        to: 'imgages',
+        flatten: true 
+      },
+    ]),
   ]
 };
