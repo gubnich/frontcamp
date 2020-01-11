84fbaf504df1082cc11bf2d7acf45b8041e8e2c8
gubnich <gubnich@yandex.ru>
2020-01-11 18:05:52 +0300

    add footer component

--- frontcamp/src/app/app.module.ts
+++ frontcamp/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
