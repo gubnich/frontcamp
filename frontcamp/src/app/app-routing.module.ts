556545a608e2ff13033e08631a597bb6b6e075b4
gubnich <gubnich@yandex.ru>
2020-01-11 18:34:06 +0300

    add contact route

--- frontcamp/src/app/app-routing.module.ts
+++ frontcamp/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainViewComponent } from './main-view/main-view.component';
+import { ContactInfoComponent } from './contact-info/contact-info.component';
 
 
 const routes: Routes = [
   { path: 'main', component: MainViewComponent },
+  { path: 'contact', component: ContactInfoComponent },
 ];
 
 @NgModule({
