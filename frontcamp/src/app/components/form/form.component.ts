eecd4f9f1b3f03b6cace0326e686e417e32ef4d9
gubnich <gubnich@yandex.ru>
2020-01-20 18:10:45 +0300

    update

--- frontcamp/src/app/components/form/form.component.ts
+++ frontcamp/src/app/components/form/form.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -8,18 +8,39 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class FormComponent implements OnInit {
   @Output() formChange = new EventEmitter();
-  form = new FormGroup({
-    heading: new FormControl(''),
-    description: new FormControl(''),
-    content: new FormControl(''),
-    imageUrl: new FormControl(''),
-    date: new FormControl(''),
-    author: new FormControl(''),
-    sourceUrl: new FormControl(''),
-  })
-  constructor() { }
+  @Input() data = {
+    title: '',
+    description: '',
+    content: '',
+    urlToImage: '',
+    publishedAt: '',
+    author: '',
+    sourceUrl: ''
+  };
+  form: FormGroup;
+  // form = new FormGroup({
+  //   heading: new FormControl(this.data.title),
+  //   description: new FormControl(''),
+  //   content: new FormControl(''),
+  //   imageUrl: new FormControl(''),
+  //   date: new FormControl(''),
+  //   author: new FormControl(''),
+  //   sourceUrl: new FormControl(''),
+  // })
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    console.log('///////////////', this.data)
+    this.form = this.fb.group({
+      heading: [this.data.title, [Validators.required]],
+      description: [this.data.description],
+      content: [this.data.content, [Validators.required]],
+      imageUrl: [this.data.urlToImage],
+      date: [new Date(this.data.publishedAt).toISOString().substr(0, 10)],
+      author: [this.data.author],
+      sourceUrl: [this.data.sourceUrl],
+
+  });
   }
 
   onHeadingChange() {
